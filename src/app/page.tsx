9bb9b83ccb7fbb146de19b9669ec4d2a0a8dfe6b
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2024-01-29 23:57:05 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import React, { useState } from 'react';
 import { MdSave, MdDeleteForever } from 'react-icons/md';
 import { IoMdCloudUpload } from "react-icons/io";
 const CustomizeImage = dynamic(() => import('@/components/CustomizeImage'), { ssr: false });
+const CustomizeImageBtn = dynamic(() => import('@/components/CustomizeImageBtn'), { ssr: false });
+const CustomizeImageFileUpload = dynamic(() => import('@/components/CustomizeImageFileUpload'), { ssr: false });
+const CustomizeImagePreview = dynamic(() => import('@/components/CustomizeImagePreview'), { ssr: false });
 
 export default function Home() {
   const [activeSide, setActiveSide] = useState('front');
@@ -36,13 +39,19 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <div className="flex flex-col md:flex-row gap-5 p-5 max-w-md">
+    <div className="flex flex-col items-center justify-center gap-3 min-h-screen py-2">
+      <div className="flex flex-col md:flex-row gap-5 p-5 max-w-xl">
         <div className="flex md:flex-col gap-2 w-20 md:w-full">
           <CustomizeImageBtn onClick={() => setActiveSide('front')} title="Front" className="font-semibold">Front</CustomizeImageBtn>
           <CustomizeImageBtn onClick={() => setActiveSide('back')} title="Back" className="font-semibold">Back</CustomizeImageBtn>
           <CustomizeImageBtn onClick={() => setActionType('delete-' + activeSide as any)} title="Delete" className="flex items-center justify-center"><MdDeleteForever size={20} /></CustomizeImageBtn>
           <CustomizeImageBtn onClick={() => setActionType('download-' + activeSide as any)} title="Download" className="flex items-center justify-center"><MdSave size={20} /></CustomizeImageBtn>
+          {
+            layerImageFront && <CustomizeImagePreview image={layerImageFront} />
+          }
+          {
+            layerImageBack && <CustomizeImagePreview image={layerImageBack} />
+          }
         </div>
         <div className="flex-1 flex flex-col gap-5">
           <div className={activeSide === 'front' ? '' : 'hidden'}>
@@ -66,37 +75,12 @@ export default function Home() {
           </CustomizeImageFileUpload>
         </div>
       </div>
+      <p
+        className="text-sm text-gray-500 px-2 md:px-0 text-center"
+        title="Created by Saurav Hathi"
+      >
+        T-Shirt Customization tool made with <a href="https://konvajs.org/" target="_blank" rel="noopener noreferrer" className="underline">Konva.js</a> and <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer" className="underline">Next.js</a> by <a href="lkjnjn" target="_blank" rel="noopener noreferrer" className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 font-bold">Saurav Hathi</a>
+      </p>
     </div >
-  )
-}
-
-const CustomizeImageBtn = ({ children, onClick, title, className }: { children: React.ReactNode, onClick: () => void, title: string, className?: string }) => {
-  return (
-    <button
-      className={"border border-dashed border-blue-500 text-blue-500 px-4 py-2 rounded-md cursor-pointer " + className}
-      onClick={onClick}
-      title={title}
-    >
-      {children}
-    </button>
-  )
-}
-
-const CustomizeImageFileUpload = ({ children, onChange, title, className, name }: { children: React.ReactNode, onChange: (e: React.ChangeEvent<HTMLInputElement>) => void, title: string, className?: string, name: string }) => {
-  return (
-    <label
-      htmlFor="file-upload"
-      className={"flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 cursor-pointer hover:shadow-md " + className}
-      title={title}>
-      {children}
-      <input
-        type="file"
-        accept="image/*"
-        id="file-upload"
-        name={name}
-        onChange={onChange}
-        className="hidden"
-      />
-    </label>
   )
 }
